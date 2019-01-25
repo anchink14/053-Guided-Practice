fc1ed0cecc22eca805e0847be679ef7cb8d1df5d
anchink14 <44591687+anchink14@users.noreply.github.com>
2019-01-24 16:42:29 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+    $("#replyYes").click(function(){
+    $("#homers-reply").text("Whoo Hoo");
+    $("img").css("background-color","blue");
+    $("body").css("background-color","red");
+    $("h1").css("color","yellow");});
+$("#replyNo").click(function() {
+    $("#homers-reply").text("Doh!");
+    $("img").css("background-color","red");
+    $("body").css("background-color","blue");
+    $("h1").css("color","green");
+    
+    
+    
+});
